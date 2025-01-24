0575df915dad643c36d460b07617d510b781bc59
tonyz840101 <tonyz840101@gmail.com>
2025-01-25 03:07:02 +0800

    Add initial files for testViewer including HTML, TypeScript, and package configuration

---
+++ export-gh-pages/src/index.ts
@@ -0,0 +1,5 @@
+import { CardGroup } from "core/group";
+import { CardSearcher } from "core/CardSearcher";
+import CardList from "../../data/cardList.json";
+
+var CardSearcherInstance = new CardSearcher(CardList);
